2cf0bbf556e9071ca8130bee8dce0365ab831995
Saqlain Khan <iamsaqlain25@gmail.com>
2021-12-06 16:51:13 +0530

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,13 @@
 const request = require("supertest");
 const app = require("../../app");
+const { loadPlanetsData } = require("../../model/planets.model");
 
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
